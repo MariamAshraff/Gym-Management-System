b19c00847fb519ba779793c2eb28f7551dc16e6d
Mariam Ashraf <mariamashrafff163@gmail.com>
2026-04-23 22:40:59 +0200

    Payment

---
+++ Gym Management System/Data/Models/Payment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Management_System.Data.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int TraineeSubscriptionId { get; set; }
+        public virtual TraineeSubscription TraineeSubscription { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Amount { get; set; }
+
+        public DateTime PaidAt { get; set; } = DateTime.Now;
+
+        [Required, MaxLength(20)]
+        public string Method { get; set; } // Cash - Card
+    }
+}
---
+++ Gym Management System/Migrations/20260423203858_RelationBetweenTraineeAndSubscriptionAndPayment.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gym_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationBetweenTraineeAndSubscriptionAndPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TraineeSubscriptionId = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PaidAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Method = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_TraineeSubscriptions_TraineeSubscriptionId",
+                        column: x => x.TraineeSubscriptionId,
+                        principalTable: "TraineeSubscriptions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_TraineeSubscriptionId",
+                table: "Payments",
+                column: "TraineeSubscriptionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+        }
+    }
+}
